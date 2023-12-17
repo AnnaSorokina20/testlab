a257e386e2c3c4347070b4b426c5a8c7d92a1832
AnnaSorokina20 <a.v.sorokina@student.khai.edu>
2023-12-17 18:32:08 +0100

    Input info

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Anna");
 Console.WriteLine("20.07.05");
+
+Console.WriteLine("What is your name?");
+string input = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+if (int.TryParse(Console.ReadLine(), out int age))
+{
+    Console.WriteLine("You entered an integer for age: " + age);
+}
+else
+{
+    Console.WriteLine("That's not a valid integer for age.");
+}
